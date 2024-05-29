ba1d52ab4617db262f0fa910744823ab6316950a
Felipe <felipeassis16@hotmail.com>
2024-05-29 03:14:26 -0300

    commit

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./ProgressBar.css";
 
 const ProgressBar = ({ currentStep }) => {
-  const totalSteps = 30; // Total de steps no quiz
+  // const totalSteps = 30; // Total de steps no quiz
   const steps = [2, 8, 12, 18, 24, 30];
-  const stepIntervals = totalSteps / (steps.length - 1);
+  // const stepIntervals = totalSteps / (steps.length - 1);
 
   return (
     <div className="progress-bar">
